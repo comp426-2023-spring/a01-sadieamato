2ced8ec61d4dd5bc8b62492b8493ad47f1571e72
Sadie Amato <saamato@email.unc.edu>
2023-01-19 17:34:09 -0500

    Added minimist args and default port

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 // Require http module
 var http = require('http');
 // Require fs module
-var fs = require('fs')
+var fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
-var minimist = require('minimist')
+var minimist = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-
+let args = minimist(process.argv.slice(1), {
+    default: {
+        port: 3000
+    },
+});
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = // FILL IN ;
+const port = args.port;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
