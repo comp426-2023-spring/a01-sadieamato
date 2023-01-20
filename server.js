b8fab78ece4083c859b40586d365c4f8f9a70865
Sadie Amato <saamato@email.unc.edu>
2023-01-20 10:48:51 -0500

    rearranged write head and set header

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ fs.readFile('./public/index.html', (err, data) => {
         console.error(err);
         return;
     }
-    var file_data = data;
- })
+
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
@@ -30,12 +29,14 @@ fs.readFile('./public/index.html', (err, data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 const server = http.createServer((req, res) => {
-  res.statusCode(200)
   res.setHeader('Content-Type', 'text/html');
-  res.end(file_data);
+  res.writeHead(200);
+  res.end(data);
 });
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port);
 console.log(`Server listening on port ${port}`);
+
+});
 // That's it! You're all done!
